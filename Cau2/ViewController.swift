08e4f6914f4af45a7a225457eee2e596d38f3494
Cntt08 <Admin08@Cntt08s-iMac.local>
2017-07-05 15:11:52 +0700

    finished

--- Cau2/ViewController.swift
+++ Cau2/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textURL1: UITextField!
+    @IBOutlet var textURL2: UITextField!
+    
+    @IBOutlet var image1: UIImageView!
+    @IBOutlet var image2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textURL1.text = ""
+        textURL2.text = ""
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,51 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func loadImage(_ sender: Any) {
+        
+        if(textURL1.text == "" && textURL2.text == ""){
+            
+        }
+        else{
+            let configuration =  URLSession(configuration: URLSessionConfiguration.default)
+            if(textURL1.text != ""){
+                let thread1 = DispatchQueue(label: "image1")
+                 let url1: URL = URL(string: textURL1.text!)!
+                thread1.async {
+                    configuration.dataTask(with: url1, completionHandler: { (dataImage, response, error) in
+                        if let data = dataImage {
+                            DispatchQueue.main.async {
+                                self.image1.image = UIImage(data: data)
+                            }
+                        }
+                    }).resume()
+                }
+
+
+            }
+            if(textURL2.text != ""){
+                       let thread2 = DispatchQueue(label: "image2")
+            
+           
+           
+            let url2: URL = URL(string: textURL2.text!)!
+            
+            
+            
+            thread2.async {
+                configuration.dataTask(with: url2, completionHandler:{ (dataImage, response, error) in
+                    if let data = dataImage {
+                        DispatchQueue.main.async {
+                            self.image2.image = UIImage(data: data)
+                        }
+                    }
+                }).resume()
+            }
+            }
+            
+        }
+    }
+    
 
 }
 
